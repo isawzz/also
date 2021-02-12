cfe70e3e1c3070c86ed4868beb5c95faba93e0b8
isawzz <iwgajam@gmail.com>
2021-02-12 06:09:12 -0800

    first commit

---
+++ BASE/_globals.js
@@ -0,0 +1,20 @@
+var Username,Gamename,Tablename;
+
+var I; //running instance
+var Live, DB, G, T, P, U, User, Settings, SettingsChanged; //, G...Game, T...Table, U...Userdata
+
+var Speech;
+
+var Pictures, Goal, Selected, Score;
+
+var TO; //timeout dictionary
+
+var uiActivated, auxOpen;
+
+var AD, ADS; //addons: current instance and dict of all existing addons (form DB)
+
+var UIS = {}; 
+
+var App; //not sure!
+var Daat = {}; //some kind of global object for testing ...
+
