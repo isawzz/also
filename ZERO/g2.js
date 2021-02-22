05bd031e39635190366d787da9f00d0857e55f12
isawzz <iwgajam@gmail.com>
2021-02-22 12:25:15 -0800

    das

--- ZERO/g2.js
+++ ZERO/g2.js
@@ -1,3 +1,4 @@
+function getAllItems(cond,baseSet='all'){	return getItems(10000,cond,baseSet);}
 function getItems(n, cond, baseSet = 'all') {
 	//n ... number, key list, info list or item list
 	//cond ... undefined, string(KeySet or search SymKeys) or function(filter SymKeys)
@@ -7,7 +8,7 @@ function getItems(n, cond, baseSet = 'all') {
 		isdef(KeySets[cond]) ? KeySets[cond] : baseSet.filter(x => x.includes(cond))
 		: baseSet.filter(x => cond(Syms[x])) : baseSet;
 	//console.log('keys', keys);
-	if (isNumber(n)) n = choose(keys, n);
+	if (isNumber(n)) n = n>=keys.length?keys:choose(keys, n);
 	if (isString(n[0])) n = n.map(x => Syms[x]);
 	if (nundef(n[0].info)) n = n.map(x => infoToItem(x));
 	return n;
