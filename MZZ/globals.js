e0e699db1488d316d86f22667e9e5c3537050691
isawzz <iwgajam@gmail.com>
2021-07-06 13:24:56 -0700

    das

--- MZZ/globals.js
+++ MZZ/globals.js
@@ -1,10 +1,10 @@
 //#region config
 const USELIVESERVER = true; //brauch ich DOCH!!!nicht! solange node nicht rennt kein problem!!!
+var START_IN_MENU = true; //!BROADCAST_SETTINGS;
+const DEFAULTUSERNAME = 'gul'; //USELIVESERVER?'nil':'gul';// BROADCAST_SETTINGS?'nil':'gul'; // nil | gul | felix
 
 var USE_LOCAL_STORAGE = true; // !BROADCAST_SETTINGS; // true | false //localStorage is cleared when false!!!!!
 const CLEAR_LOCAL_STORAGE = false; //BROADCAST_SETTINGS;
-var START_IN_MENU = true; //!BROADCAST_SETTINGS;
-const DEFAULTUSERNAME = 'gul'; //USELIVESERVER?'nil':'gul';// BROADCAST_SETTINGS?'nil':'gul'; // nil | gul | felix
 var USE_ADDONS = false;
 
 // const OFFLINE = BROADCAST_SETTINGS;
